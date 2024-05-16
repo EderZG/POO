e0f0c9c7cc32dcd72b2995bae3365f0696988a20
Soy Eder <eder.eyzg@gmail.com>
2024-05-16 15:02:32 -0600

    Atributos y metodos

--- app/src/main/java/com/example/poo/MainActivity.kt
+++ app/src/main/java/com/example/poo/MainActivity.kt
@@ -16,5 +16,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var num: Int = 0
+        var jota: Person = Person()
+        jota.die()
+        println(jota.alive)
     }
 }
---
+++ app/src/main/java/com/example/poo/Person.kt
@@ -0,0 +1,9 @@
+package com.example.poo
+
+class Person {
+    var alive: Boolean = true
+
+    fun die(){
+        alive = false
+    }
+}
