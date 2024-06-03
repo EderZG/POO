0997cc577f796e1098fe30c45f54d0e6974e3e01
Soy Eder <eder.eyzg@gmail.com>
2024-06-02 21:55:51 -0600

    NumClass

--- app/src/main/java/com/example/poo/MainActivity.kt
+++ app/src/main/java/com/example/poo/MainActivity.kt
@@ -37,6 +37,17 @@ class MainActivity : AppCompatActivity() {
         var sol : star = star("Sol",696340f,"Via lactea")
         println(sol)
 
+        var hoy:dias = dias.LUNES
+        var semana = dias.values()
+        for (i in semana) println(i)
+
+        println(dias.valueOf("MIERCOLES"))
+        println(hoy.name)
+        println(hoy.ordinal)
+
+        println(hoy.saludo())
+        println(hoy.laboral)
+        println(hoy.jornada)
 
         /*
         var num: Int = 0
